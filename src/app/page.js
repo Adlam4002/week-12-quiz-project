4e27918508d9f945da7aea3414f97e39d106b23a
Alex <adlam4002@hotmail.co.uk>
2024-08-01 14:44:47 +0100

    authentication as a group

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,13 @@
+import { Logout } from "@/components/Logout";
+import { SignInButton } from "@/components/SignIn";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="text-white">
       <h1>Hello Guys!</h1>
+      <SignInButton />
+      <Logout />
     </main>
   );
 }
