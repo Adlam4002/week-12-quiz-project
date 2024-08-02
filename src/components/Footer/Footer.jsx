b5264fd613a09df4690e8b279caaa3736d3e2885
Zilzaer <josh.e.g.day@gmail.com>
2024-08-02 16:43:11 +0100

    Footer Component + adjustments
    Finished the Footer Component including corrected external links, Centered accordian component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,32 +1,56 @@
-import Link from 'next/link';
-import Image from 'next/image';
-import * as Accordion from '@radix-ui/react-accordion';
-import styles from './styles.module.css';
+import Link from "next/link";
+import Image from "next/image";
+import * as Accordion from "@radix-ui/react-accordion";
+import styles from "./styles.module.css";
 
 const Footer = () => {
   return (
     <footer className={`${styles.footer}`}>
-      
-      <Accordion.Root className={`${styles.accordion}`} type="single" collapsible>
-        <Accordion.Item className={`${styles.accordionItem}`} value="item-1">
-          <Accordion.Header className={`${styles.accordionHeader}`}>
-            <Accordion.Trigger className={`${styles.accordionTrigger}`}>
-              About Us
-            </Accordion.Trigger>
-          </Accordion.Header>
-          <Accordion.Content className={`${styles.accordionContent}`}>
-          <nav>
-        <ul className={`${styles.ul}`}>
-          <h1>Alex Adlam - Daniel Szabo - Joshua Day - Week 12 Assignment - Student Demo</h1>
-          <a href="/">GitHub</a>
-          <a href="/">LinkedIn-Alex</a>
-          <a href="/">LinkedIn-Daniel</a>
-          <a href="/">LinkedIn-Josh</a>
-        </ul>
-      </nav>
-          </Accordion.Content>
-        </Accordion.Item>
-      </Accordion.Root>
+      <div className={`${styles.layout}`}>
+        <h1>
+          Alex Adlam - Daniel Szabo - Joshua Day - Week 12 Assignment - Student
+          Demo
+        </h1>
+
+        <Accordion.Root
+          className={`${styles.accordion}`}
+          type="single"
+          collapsible
+        >
+          <Accordion.Item className={`${styles.accordionItem}`} value="item-1">
+            <Accordion.Header className={`${styles.accordionHeader}`}>
+              <Accordion.Trigger className={`${styles.accordionTrigger}`}>
+                About Us
+              </Accordion.Trigger>
+            </Accordion.Header>
+            <Accordion.Content className={`${styles.accordionContent}`}>
+              <nav>
+                <ul className={`${styles.ul}`}>
+                  <a className={`${styles.links}`} href="https://github.com/Adlam4002/week-12-quiz-project">
+                    <Image
+                      src="/Assets/github-mark-white.png"
+                      alt="GitHub Logo"
+                      width={50}
+                      height={50}
+                    />
+                  </a>
+
+                  <h2>LinkedIn:</h2>
+                  <a href="https://www.linkedin.com/in/alex-adlam-3456b3a5/">
+                    Alex
+                  </a>
+                  <a href="https://www.linkedin.com/in/daniel-szabo-455134248/">
+                    Daniel
+                  </a>
+                  <a href="https://www.linkedin.com/in/joshua-day-ba1652311/">
+                    Josh
+                  </a>
+                </ul>
+              </nav>
+            </Accordion.Content>
+          </Accordion.Item>
+        </Accordion.Root>
+      </div>
     </footer>
   );
 };
