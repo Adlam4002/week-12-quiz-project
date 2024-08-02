c418e4619d4312f51822d693d15c0826c31ef667
Daniel G Szabo <d.g.szabo-2017@hull.ac.uk>
2024-08-02 11:38:18 +0100

    fix for main page

--- src/app/page.js
+++ src/app/page.js
@@ -6,18 +6,20 @@ import Header from "@/components/Header/Header";
 
 export default function Home() {
   return (
-    <Header></Header>
-    <main className="text-center text-lg font-bold text-white flex flex-col items-center">
-      {" "}
-      {/* border-solid border-2 */}
-      <div className="flex flex-col items-center w-72">
-        <h1>Welcome to QuizzyPop!</h1>
-        <QuizzComponent />
-        {/* Quizz component here */}
-        {/* <h1>Hello Guys!</h1>
+    <>
+      <Header></Header>
+      <main className="text-center text-lg font-bold text-white flex flex-col items-center">
+        {" "}
+        {/* border-solid border-2 */}
+        <div className="flex flex-col items-center w-72">
+          <h1>Welcome to QuizzyPop!</h1>
+          <QuizzComponent />
+          {/* Quizz component here */}
+          {/* <h1>Hello Guys!</h1>
       <SignInButton />
       <Logout /> */}
-      </div>
-    </main>
+        </div>
+      </main>
+    </>
   );
 }
