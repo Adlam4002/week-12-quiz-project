1294ae47baee77b70c9d7d6e4f5a80a280ee1f85
Daniel G Szabo <d.g.szabo-2017@hull.ac.uk>
2024-08-02 11:34:50 +0100

    Update background color and font in layout and quizz components

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,21 @@
 import { Logout } from "@/components/Logout";
+import QuizzComponent from "@/components/QuizzComponent";
 import { SignInButton } from "@/components/SignIn";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="text-white">
-      <h1>Hello Guys!</h1>
+    <main className="text-center text-lg font-bold text-white flex flex-col items-center">
+      {" "}
+      {/* border-solid border-2 */}
+      <div className="flex flex-col items-center w-72">
+        <h1>Welcome to QuizzyPop!</h1>
+        <QuizzComponent />
+        {/* Quizz component here */}
+        {/* <h1>Hello Guys!</h1>
       <SignInButton />
-      <Logout />
+      <Logout /> */}
+      </div>
     </main>
   );
 }
