79ce16831eb86f25c7432b246522c2cc7f6b6ff7
Zilzaer <josh.e.g.day@gmail.com>
2024-08-02 11:04:46 +0100

    Created Header Component
    Created a header component that displays the log in/ signout button depending on users session state

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,13 @@
 import { Logout } from "@/components/Logout";
 import { SignInButton } from "@/components/SignIn";
 import Image from "next/image";
+import Header from "@/components/Header/Header";
 
 export default function Home() {
   return (
     <main className="text-white">
+      <Header></Header>
       <h1>Hello Guys!</h1>
-      <SignInButton />
-      <Logout />
     </main>
   );
 }
