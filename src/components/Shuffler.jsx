769d7fd6b44cef920ff2994101cdbc0ac1cbd530
Alex <adlam4002@hotmail.co.uk>
2024-08-04 16:21:42 +0100

    managed to get the shuffle working using a known shuffle function and then took steps to ensure I could track the correct answer through the shuffle and then for the correct answer to be rendered in a div with an ID letting me know for visual effects to be applied

---
+++ src/components/Shuffler.jsx
@@ -0,0 +1,39 @@
+export default function Shuffler({ answers }) {
+  let check = answers.slice();
+  // the function bellow is the Durstenfeld shuffle function which is an optimsed version of the Fisher-Yates shuffle. It shuffles in place to so check the correct answer I sliced the array above to have a copy of the original order.
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      let temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+  }
+  shuffleArray(answers);
+  console.log(`OG: ${check}`);
+  console.log(`Shuf: ${answers}`);
+  return (
+    <>
+      {answers[0] == check[0] ? (
+        <div id="profile-correct-answer">{answers[0]}</div>
+      ) : (
+        <div>{answers[0]}</div>
+      )}
+      {answers[1] == check[0] ? (
+        <div id="profile-correct-answer">{answers[1]}</div>
+      ) : (
+        <div>{answers[1]}</div>
+      )}
+      {answers[2] == check[0] ? (
+        <div id="profile-correct-answer">{answers[2]}</div>
+      ) : (
+        <div>{answers[2]}</div>
+      )}
+      {answers[3] == check[0] ? (
+        <div id="profile-correct-answer">{answers[3]}</div>
+      ) : (
+        <div>{answers[3]}</div>
+      )}
+    </>
+  );
+}
