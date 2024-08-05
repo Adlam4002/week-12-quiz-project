3365f03a1bb279ac510bc4a01c4f89c731f382cc
Alex <adlam4002@hotmail.co.uk>
2024-08-05 16:17:56 +0100

    did a little styling and dangerousHMTL for the question list and fiex caps on form

--- src/app/questionslist/page.js
+++ src/app/questionslist/page.js
@@ -3,17 +3,21 @@ import Link from "next/link";
 
 export default async function QuestionsListPage() {
   let response = await db.query(`
-       SELECT * FROM quiz_questions 
-        `);
+         SELECT * FROM quiz_questions
+          `);
   const data = response.rows;
-  //   console.log(data);
+  console.log(data);
   return (
-    <main className="flex flex-col items-center">
+    <main id="question-list-page" className="flex flex-col items-center ">
       <div className="flex flex-col items-center">
         {data.map((item) => (
           <>
             <Link href={`/questionslist/${item.id}`} key={item.id}>
-              <p dangerouslySetInnerHTML={{ __html: item.question }}></p>
+              <p
+                className="text-2xl"
+                id="question-list"
+                dangerouslySetInnerHTML={{ __html: item.question }}
+              ></p>
             </Link>
             <br />
           </>
