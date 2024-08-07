d0c78b2f715d31e54a3b1e0802153e33194aa352
Daniel G Szabo <d.g.szabo-2017@hull.ac.uk>
2024-08-07 19:20:27 +0100

    Update header and footer styles to use flexbox and add backdrop filter for a blurred effect

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,32 +1,25 @@
 import { auth } from "@/auth";
-import { useSession } from "next-auth/react"; // Make sure to import useSession from "next-auth/react"
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 import Image from "next/image";
 import { Logout } from "@/components/Logout";
 import { SignInButton } from "@/components/SignIn";
-// import { motion } from "framer-motion";
-// import React, { createContext } from 'react';
-
-import styles from "./styles.module.css";
 
 export default async function Header() {
   const session = await auth();
 
   if (!session) {
     return (
-      </*motion.*/ header
-        className={`${styles.header}`}
-        initial={{ scale: 1 }}
-        animate={{ scale: 1.05 }}
-        transition={{ duration: 2, repeat: Infinity, repeatType: "reverse" }}
-      >
+      <header className="w-full bg-white bg-opacity-30 backdrop-blur-md text-slate-800 flex justify-around items-center p-4 h-16 shadow-md">
         <div>
-          <Image
-            src="/Assets/QuizziePop.png"
-            alt="GitHub Logo"
-            width={500}
-            height={70}
-          />
+          <Link href="/">
+            <Image
+              src="/Assets/QuizziePop.png"
+              alt="GitHub Logo"
+              width={200}
+              height={35}
+            />
+          </Link>
         </div>
         <SignInButton />
       </header>
@@ -34,46 +27,49 @@ export default async function Header() {
   }
 
   return (
-    <header className={`${styles.header}`}>
+    <header className="w-full bg-white bg-opacity-30 backdrop-blur-md text-slate-800 flex justify-around items-center p-4 h-16 shadow-md">
       <div>
-        <Image
-          src="/Assets/QuizziePop.png"
-          alt="GitHub Logo"
-          width={500}
-          height={70}
-        />
+        <Link href="/">
+          <Image
+            src="/Assets/QuizziePop.png"
+            alt="GitHub Logo"
+            width={200}
+            height={35}
+          />
+        </Link>
       </div>
-      <nav className="">
-        <ul className={`${styles.ul}`}>
-          <li>
-            <button className={`${styles.button}`}>
-              <Link href="/">Home</Link>
-            </button>
-          </li>
-          <li>
-            <button className={`${styles.button}`}>
-              <Link href="/randomq">Random Question</Link>
-            </button>
-          </li>
-          <li>
-            <button className={`${styles.button}`}>
-              <Link href="/newquestion">Submit a Question</Link>
-            </button>
-          </li>
-          <li>
-            <button className={`${styles.button}`}>
-              <Link href="/questionslist">View Questions</Link>
-            </button>
-          </li>
-          <li>
-            <button className={`${styles.button}`}>
-              <Link href={`/userprofile/${session.user.id}`}>My Profile</Link>
-            </button>
-          </li>
-          <li>
-            <Logout />
-          </li>
-        </ul>
+      <nav className="flex flex-row gap-4">
+        <Link
+          href="/"
+          className="px-3 py-2 rounded-lg hover:bg-white hover:bg-opacity-50"
+        >
+          Home
+        </Link>
+        <Link
+          href="/randomq"
+          className="px-3 py-2 rounded-lg hover:bg-white hover:bg-opacity-50"
+        >
+          Random Question
+        </Link>
+        <Link
+          href="/newquestion"
+          className="px-3 py-2 rounded-lg hover:bg-white hover:bg-opacity-50"
+        >
+          Submit a Question
+        </Link>
+        <Link
+          href="/questionslist"
+          className="px-3 py-2 rounded-lg hover:bg-white hover:bg-opacity-50"
+        >
+          View Questions
+        </Link>
+        <Link
+          href={`/userprofile/${session.user.id}`}
+          className="px-3 py-2 rounded-lg hover:bg-white hover:bg-opacity-50"
+        >
+          My Profile
+        </Link>
+        <Logout />
       </nav>
     </header>
   );
